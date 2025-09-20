4ecef9926c84fd970fec8fac18860d6412b9acc6
JNMC0 <20243ds126@utez.edu.mx>
2025-09-19 18:30:22 -0600

    agregue algo nuevo

--- src/App.java
+++ src/App.java
@@ -2,10 +2,11 @@ public class App {
  
     public static void main(String[] args) throws Exception {
     int n=5;
+     int resultado = suma(n);
         for(int i=0; i<=n; i++){
         System.out.println(fibo(i));
     }
-    
+    System.out.println("suma de(" + n + ") es ="  + resultado);
     }
    public static int fibo(int n){
         if (n<=1){
@@ -14,6 +15,15 @@ public class App {
         }
       return fibo(n-1)+fibo(n-2);
 
+    }
+      public static int suma(int n) {
+        if (n == 0) {
+            return 0; 
+        } else {
+            return n + suma(n - 1); 
+            
+        }
+        
     }
 }
 
